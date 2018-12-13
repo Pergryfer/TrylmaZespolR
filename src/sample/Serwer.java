2f7f9a01e12afadbeae7906915d0b658b11df2e6
Swietlon <44343513+Swietlon@users.noreply.github.com>
2018-12-13 17:25:03 +0100

    dodano klient i serwer

---
+++ src/sample/Serwer.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Serwer {
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(9090);
+        try {
+            while(true) {
+                Socket socket = serverSocket.accept();
+                try {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                } finally {
+                    socket.close();
+                }
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
